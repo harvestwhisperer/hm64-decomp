ee7a791b1c969d3b5c98e73dfb6d1b8b3354e18a
harvestwhisperer <142757116+harvestwhisperer@users.noreply.github.com>
2025-09-01 21:05:00 -0700

    Delete src/data/animation directory

--- src/data/animation/animationScripts.h
+++
@@ -1,8 +0,0 @@
-#define ANIM_TYPE_ONE_SHOT           0x0000  // type 0xFF
-#define ANIM_TYPE_LOOP               0x2000  // type 0xFE
-#define ANIM_TYPE_DESTROY_ON_END     0x4000  // type 0xFD
-
-#define FLIP_HORIZONTAL              0x8000
-
-#define PACK_ANIM_DATA(metadata_offset, anim_type, horizontal_flip) \
-    ((metadata_offset) | (anim_type) | (horizontal_flip))
